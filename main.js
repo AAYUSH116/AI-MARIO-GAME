0dc3ca5ea6095e3d86388fe2a0d920bf1c261f68
AAYUSH116 <82816625+AAYUSH116@users.noreply.github.com>
2021-08-05 17:41:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,12 +6,13 @@ function preload() {
 
 function setup() {
 	canvas = createCanvas(1240,336);
-	instializeInSetup(mario);
 	canvas.parent('canvas');
+	instializeInSetup(mario);
+	
 
 	video=createCapture(VIDEO);
 	video.size(800,400);
-	video.parent('game_console')
+	video.parent('game_console');
 
 	poseNet=ml5.poseNet(video,modelLoaded);
 	poseNet.on('pose',gotPoses);
@@ -28,7 +29,7 @@ if(results.length>0){
 }
 }
 function draw() {
-	game()
+	game();
 }
 
 
